f0dc7c5be8ada35f25c8277b83e8fd8a0e01773a
Agustina Rojas <agustina.rojas193292@alumnos.info.unlp.edu.ar>
2022-10-24 11:26:07 -0300

    punto 7 y 8

---
+++ PRACTICA 4/Ejercicio7.sql
@@ -0,0 +1,95 @@
+/* Ejercicio 7:
+Banda(codigoB, nombreBanda, genero_musical, año_creacion)
+Integrante (DNI, nombre, apellido,dirección,email, fecha_nacimiento,codigoB(fk))
+Escenario(nroEscenario, nombre _ escenario, ubicación,cubierto, m2, descripción)
+Recital(fecha,hora,nroEscenario, codigoB (fk))
+ */
+/*  1. Listar DNI, nombre, apellido,dirección y email de integrantes nacidos entre 1980 y 1990 y
+hayan realizado algún recital durante 2018. */
+
+SELECT i.DNI, i.nombre, i.apellido, i.direccion
+FROM Integrante i
+WHERE i.fecha_nacimiento BETWEEN "01/01/1980" AND "31/12/1990" AND i.DNI IN (
+    SELECT i.DNI
+    FROM Recital r INNER JOIN Banda b ON (r.codiboB = b.codigoB)
+        INNER JOIN Integrante i ON (b.codigoB = i.codigoB)
+    WHERE r.fecha BETWEEN "01/01/2018" AND "31/12/2018"
+)
+
+/* 2. Reportar nombre, género musical y año de creación de bandas que hayan realizado
+recitales durante 2018, pero no hayan tocado durante 2017 . */
+
+SELECT b.nombreBanda, b.genero_musical, b.año_creacion
+FROM Banda b INNER JOIN Recital r ON (b.codigoB = r.codigoB)
+WHERE r.fecha BETWEEN "01/01/2018" AND "31/12/2018"
+EXCEPT(
+    SELECT b.nombreBanda, b.genero_musical, b.año_creacion
+    FROM Banda b INNER JOIN Recital r ON (b.codigoB = r.codigoB)
+    WHERE r.fecha BETWEEN "01/01/2017" AND "31/12/2017"
+)
+
+/* 3. Listar el cronograma de recitales del dia 04/12/2018. Se deberá listar: nombre de la banda
+que ejecutará el recital, fecha, hora, y el nombre y ubicación del escenario correspondiente. */
+
+SELECT b.nombre, r.fecha, r.hora, e.nombre_escenario, e.ubicación
+FROM Recital r INNER JOIN Banda b ON (r.codigoB = b.codigoB)
+    INNER JOIN Escenario e ON (r.nroEscenario = e.nroEscenario)
+WHERE r.fecha = "04/12/2018"
+
+/* 4. Listar DNI, nombre, apellido,email de integrantes que hayan tocado en el escenario con
+nombre ‘Gustavo Cerati’ y en el escenario con nombre ‘Carlos Gardel’. */
+
+SELECT i.DNI, i.nombre, i.apellido, i.email
+FROM Recital r INNER JOIN Integrante i ON (r.codigoB = i.codigoB)
+    INNER JOIN Escenario e ON (r.nroEscenario = e.nroEscenario)
+WHERE e.nombre_escenario = "Gustavo Cerati"
+INTERSECT(
+    SELECT i.DNI, i.nombre, i.apellido, i.email
+    FROM Recital r INNER JOIN Integrante i ON (r.codigoB = i.codigoB)
+        INNER JOIN Escenario e ON (r.nroEscenario = e.nroEscenario)
+    WHERE e.nombre_escenario = "Carlos Gardel"
+) 
+
+/* 5. Reportar nombre, género musical y año de creación de bandas que tengan más de 8
+integrantes. */
+
+SELECT b.nombreBanda, b.genero_musical, b.año_creacion
+FROM Banda b INNER JOIN Integrante i ON (b.codigoB = i.codigoB)
+GROUP BY b.codigoB, b.nombreBanda, b.genero_musical, b.año_creacion
+HAVING COUNT(*) > 8
+
+/* 6. Listar nombre de escenario, ubicación y descripción de escenarios que solo tuvieron
+recitales con género musical rock and roll. Ordenar por nombre de escenario */
+
+SELECT e.nombre_escenario, e.ubicación, e.descripción
+FROM Escenario e INNER JOIN Recital r ON (e.nroEscenario = r.nroEscenario)
+    INNER JOIN Banda b ON (r.codigoB = b.codigoB)
+WHERE b.genero_musical = "rock and roll" 
+EXCEPT (
+    SELECT e.nombre_escenario, e.ubicación, e.descripción
+    FROM Escenario e INNER JOIN Recital r ON (e.nroEscenario = r.nroEscenario)
+        INNER JOIN Banda b ON (r.codigoB = b.codigoB)
+    WHERE b.genero_musical <> "rock and roll"
+)
+ORDER BY e.nombre_escenario
+
+/* 7. Listar nombre, género musical y año de creación de bandas que hayan realizado recitales
+en escenarios cubiertos durante 2018.// cubierto es true, false según corresponda */
+
+SELECT b.nombreBanda, b.genero_musical, b.año_creacion
+FROM Escenario e INNER JOIN Recital r ON (e.nroEscenario = r.nroEscenario)
+    INNER JOIN Banda b ON (r.codigoB = b.codigoB)
+WHERE (e.cubierto = true) AND (r.fecha BETWEEN "01/01/2018" AND "31/12/2018") 
+
+/* 8. Reportar para cada escenario, nombre del escenario y cantidad de recitales durante 2018. */
+
+--Debo tener en cuenta que no hubo recitales para un escenario si no lo menciona?
+
+SELECT e.nombre_escenario, COUNT(*) as Cantidad
+FROM Escenario e LEFT JOIN Recital r ON (e.nroEscenario = r.nroEscenario)
+WHERE (r.fecha BETWEEN "01/01/2018" AND "31/12/2018")
+GROUP BY e.nroEscenario, e.nombre_escenario
+
+/* 9. Modificar el nombre de la banda ‘Mempis la Blusera’ a: ‘Memphis la Blusera’. */
+
+UPDATE Banda SET nombreBanda="Memphis la Blusera" WHERE nombreBanda = "Mempis la Blusera"
---
+++ PRACTICA 4/Ejercicio8.sql
@@ -0,0 +1,132 @@
+/* Ejercicio 8- Dadas las siguientes relaciones
+Equipo(codigoE, nombreE, descripcionE)
+Integrante (DNI, nombre, apellido,ciudad,email, telefono,codigoE(fk))
+Laguna(nroLaguna, nombreL, ubicación,extension, descripción)
+TorneoPesca(codTorneo, fecha,hora,nroLaguna(fk), descripcion)
+Inscripcion(codTorneo,codigoE,asistio, gano)// asistio y gano son true o false según
+corresponda */
+/* 1. Listar DNI, nombre, apellido y email de integrantes que sean de la ciudad ‘La Plata’ y estén
+inscriptos en torneos a disputarse durante 2019. */
+
+SELECT i.DNI, i.nombre, i.apellido i.ciudad, i.email
+FROM Intengrante i INNER JOIN Inscripcion ins ON (i.codigoE = ins.codigoE)
+    INNER JOIN TorneoPesca tp ON (ins.codTorneo = tp.codTorneo)
+WHERE (i.ciudad = "La Plata") AND (tp.fecha BETWEEN "01/01/2019" AND "31/12/2019")
+
+/* 2. Reportar nombre y descripción de equipos que solo se hayan inscripto en torneos de 2018. */
+
+SELECT e.nombreE, e.descripcionE
+FROM Equipo e INNER JOIN Inscripcion ins ON (e.codigoE = ins.codigoE)
+    INNER JOIN TorneoPesca tp ON (ins.codTorneo = tp.codTorneo)
+WHERE tp.fecha BETWEEN "01/01/2019" AND "31/12/2019"
+EXCEPT (
+    SELECT e.nombreE, e.descripcionE
+    FROM Equipo e INNER JOIN Inscripcion ins ON (e.codigoE = ins.codigoE)
+        INNER JOIN TorneoPesca tp ON (ins.codTorneo = tp.codTorneo)
+    WHERE NOT(tp.fecha BETWEEN "01/01/2019" AND "31/12/2019")
+)
+
+/* 3. Listar DNI, nombre, apellido,email y ciudad de integrantes que asistieron a torneos en la
+laguna con nombre ‘La Salada, Coronel Granada’ y su equipo no tenga inscripciones a
+torneos a disputarse en 2019. */
+
+SELECT i.DNI, i.nombre, i.apellido i.ciudad, i.email
+FROM Intengrante i INNER JOIN Inscripcion ins ON (i.codigoE = ins.codigoE)
+    INNER JOIN TorneoPesca tp ON (ins.codTorneo = tp.codTorneo)
+    INNER JOIN Laguna l ON (tp.nroLaguna = l.nroLaguna)
+WHERE (l.nombreL = "La Salada, Coronel Granada")
+EXCEPT(
+    SELECT i.DNI, i.nombre, i.apellido i.ciudad, i.email
+    FROM Intengrante i INNER JOIN Inscripcion ins ON (i.codigoE = ins.codigoE)
+    INNER JOIN TorneoPesca tp ON (ins.codTorneo = tp.codTorneo)
+    WHERE tp.fecha BETWEEN "01/01/2019" AND "31/12/2019"
+)
+
+/* 4. Reportar nombre, y descripción de equipos que tengan al menos 5 integrantes.Ordenar por
+nombre y descripción. */
+
+SELECT e.nombreE, e.descripcionE
+FROM Equipo e INNER JOIN Integrante i ON (e.codigoE = i.codigoE)
+GROUP BY e.codigoE, e.nombreE, e.descripcionE
+HAVING COUNT(*) >= 5
+ORDER BY e.nombreE, e.descripcionE
+
+/* 5. Reportar nombre, y descripción de equipos que tengan inscripciones en todas las lagunas. */
+
+--CONSULTAR!!!!
+
+SELECT e.nombreE, e.descripcionE
+FROM Equipo e
+WHERE NOT EXIST (
+    SELECT l.nroLaguna
+    FROM Laguna l
+    EXCEPT ( 
+        SELECT l2.nroLaguna
+        FROM Inscripcion ins2 INNER JOIN TorneoPesca2 tp2 ON (ins2.codTorneo = tp2.codTorneo) 
+            INNER JOIN Laguna l2 ON (tp2.nroLaguna = l.nroLaguna)
+        WHERE (e.codigoE = ins2.codigoE)
+    )
+)
+
+/* 6. Eliminar el equipo con código:10000. */
+
+DELETE FROM Inscripcion WHERE codigoE="10000"
+DELETE FROM Integrante WHERE codigoE="10000"
+DELETE FROM Equipo WHERE codigoE="10000"
+
+/* 7. Listar nombreL, ubicación,extensión y descripción de lagunas que no tuvieron torneos. */
+
+--Solucion 1
+
+SELECT l.nombreL, l.ubicación, l.extension, l.descripcion
+FROM Laguna l LEFT JOIN TorneoPesca tp (l.nroLaguna = tp.nroLaguna)
+WHERE tp.nroLaguna IS NULL
+
+--Solucion 2
+
+SELECT l.nombreL, l.ubicación, l.extension, l.descripcion
+FROM Laguna l 
+WHERE l.nroLaguna NOT IN (
+    SELECT tp.nroLaguna
+    FROM TorneoPesca tp
+)
+
+/* 8. Reportar nombre, y descripción de equipos que tengan inscripciones a torneos a disputarse
+durante 2019, pero no tienen inscripciones a torneos de 2018. */
+
+--Ya me viene pasando esto varias veces donde no se cual es mejor xd
+
+--Solucion 1
+
+SELECT e.nombreE, e.descripcionE
+FROM Equipo e INNER JOIN Inscripcion ins ON (e.codigoE = ins.codigoE)
+    INNER JOIN TorneoPesca tp ON (ins.codTorneo = tp.codTorneo)
+WHERE tp.fecha BETWEEN "01/01/2019" AND "31/12/2019"
+INTERSECT (
+    SELECT e.nombreE, e.descripcionE
+    FROM Equipo e INNER JOIN Inscripcion ins ON (e.codigoE = ins.codigoE)
+        INNER JOIN TorneoPesca tp ON (ins.codTorneo = tp.codTorneo)
+    WHERE NOT(tp.fecha BETWEEN "01/01/2018" AND "31/12/2018")
+)
+
+--Solucion 2
+
+SELECT e.nombreE, e.descripcionE
+FROM Equipo e INNER JOIN Inscripcion ins ON (e.codigoE = ins.codigoE)
+    INNER JOIN TorneoPesca tp ON (ins.codTorneo = tp.codTorneo)
+WHERE tp.fecha BETWEEN "01/01/2019" AND "31/12/2019"
+EXCEPT (
+    SELECT e.nombreE, e.descripcionE
+    FROM Equipo e INNER JOIN Inscripcion ins ON (e.codigoE = ins.codigoE)
+        INNER JOIN TorneoPesca tp ON (ins.codTorneo = tp.codTorneo)
+    WHERE (tp.fecha BETWEEN "01/01/2018" AND "31/12/2018")
+)
+
+/* 9. Listar DNI, nombre, apellido, ciudad y email de integrantes que ganaron algún torneo que se
+disputó en la laguna con nombre: ‘Laguna de Chascomús’. */
+
+SELECT i.DNI, i.nombre, i.apellido i.ciudad, i.email
+FROM Intengrante i INNER JOIN Inscripcion ins ON (i.codigoE = ins.codigoE)
+    INNER JOIN TorneoPesca tp ON (ins.codTorneo = tp.codTorneo)
+    INNER JOIN Laguna l ON (tp.nroLaguna = l.nroLaguna)
+WHERE l.nombreL = "Laguna de Chascomús" and ins.gano = true
